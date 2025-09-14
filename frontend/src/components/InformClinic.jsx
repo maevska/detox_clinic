9146c450cf23284bce790ad77ce9a9934c4d487e
maevska <sofiamaevskaa4@gmail.com>
2025-09-14 21:00:58 +0300

    Добавление увеличения документа и добавление стилей компонента информации

--- frontend/src/components/InformClinic.jsx
+++ frontend/src/components/InformClinic.jsx
@@ -1,6 +1,16 @@
+import { useState } from "react";
 import "../styles/InformClinic.css";
 
 const InformClinic = () => {
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    const images = [
+        { src: "/images/image 279.png", alt: "Документ 1 - лицензия" },
+        { src: "/images/image 278.png", alt: "Документ 2 - лицензия" },
+        { src: "/images/image 281.png", alt: "Документ 3 - лицензия" },
+        { src: "/images/image 280.png", alt: "Документ 4 - лицензия" },
+    ];
+
     return (
         <div className="inform-container">
             <div className="inform-all">
@@ -8,7 +18,7 @@ const InformClinic = () => {
                     <div className="text-header-inf">
                         <h2 className="inform-title">О клинике</h2>
                         <p className="inform-p">
-                            Наши квалифицированные врачи и наркологи предлагают широкий спектр наркологических услуг, 
+                            Наши квалифицированные врачи и наркологи предлагают широкий спектр наркологических услуг,
                             включая стационарное лечение, кодирование, и многие другие виды лечения. Мы заботимся о каждом пациенте
                             и гарантируем высокое качество нашей работы. Не стесняйтесь обращаться к нам в любое время, мы всегда готовы
                             оказать наркологическую помощь.
@@ -27,15 +37,27 @@ const InformClinic = () => {
                 </div>
 
                 <div className="inform-footer">
-                    <p className="doc-p">Имеем все необходимые документы для предоставления медицинских услуг:</p>
+                    <p className="doc-p">
+                        Имеем все необходимые документы для предоставления медицинских услуг:
+                    </p>
                     <div className="doc-img">
-                        <img src="/images/image 278.png" alt="Документ 1 - лицензия" />
-                        <img src="/images/image 279.png" alt="Документ 2 - лицензия" />
-                        <img src="/images/image 280.png" alt="Документ 3 - лицензия" />
-                        <img src="/images/image 281.png" alt="Документ 4 - лицензия" />
+                        {images.map((img, i) => (
+                            <img
+                                key={i}
+                                src={img.src}
+                                alt={img.alt}
+                                onClick={() => setSelectedImage(img.src)}
+                                className="clickable-img"
+                            />
+                        ))}
                     </div>
                 </div>
             </div>
+            {selectedImage && (
+                <div className="modal" onClick={() => setSelectedImage(null)}>
+                    <img src={selectedImage} alt="Увеличенная картинка" className="modal-img" />
+                </div>
+            )}
         </div>
     );
 };
