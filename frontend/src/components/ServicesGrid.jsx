cb63187b62a2da85dc2610e04e62525ca7993280
maevska <sofiamaevskaa4@gmail.com>
2025-09-17 22:55:56 +0300

    вынесение данных  карточек услуг в js объект

--- frontend/src/components/ServicesGrid.jsx
+++ frontend/src/components/ServicesGrid.jsx
@@ -1,57 +1,20 @@
 import React, { useState } from 'react';
 import "../styles/ServicesGrid.css";
 import Card from "../components/Card";
-import Rectangle from "../assets/Rectangle11.png"
-import Rectangle1 from "../assets/Rectangle12.png"
-import Rectangle2 from "../assets/Rectangle13.png"
-import Rectangle3 from "../assets/Rectangle14.png"
-import Rectangle4 from "../assets/Rectangle15.png"
-import Rectangle5 from "../assets/Rectangle16.png"
+import service from '../data/ServiceData';
 
 const ServicesGrid = () => {
     return (
         <div className="card-container">
-            <Card
-                image={ Rectangle }
-                title="Стандартная терапия"
-                description="Для облегчения похмельного синдрома и прерывания запоя. А также при алкогольном отравлении"
-                price="2400"
-            />
-
-            <Card
-                image={ Rectangle1 }
-                title="Усиленная терапия"
-                description="Рекомендуется для облегчения похмелья и прерывания запоев длительностью до недели"
-                price="5800"
-            />
-
-            <Card
-                image={ Rectangle5 }
-                title="Восстановление +"
-                description="Для облегчения похмельного синдрома и прерывания запоя. А также при алкогольном отравлении"
-                price="15 800"
-            />
-
-            <Card
-                image={ Rectangle3 }
-                title="Максимальная терапия"
-                description="Для облегчения похмельного синдрома и прерывания запоя. А также при алкогольном отравлении"
-                price="18 800"
-            />
-
-            <Card
-                image={ Rectangle4 }
-                title="Кодирование +"
-                description="Рекомендуется для облегчения похмелья и прерывания запоев длительностью до недели"
-                price="3200"
-            />
-
-            <Card
-                image={ Rectangle2 }
-                title="Выезд нарколога на дом"
-                description="Для облегчения похмельного синдрома и прерывания запоя. А также при алкогольном отравлении"
-                price="3500"
-            />
+            {service.map((service, index) => (
+                <Card
+                    key={index}
+                    image={service.image}
+                    title={service.title}
+                    description={service.description}
+                    price={service.price}
+                />
+            ))}
         </div>
     );
 };
---
+++ frontend/src/data/ServiceData.js
@@ -0,0 +1,47 @@
+import Rectangle from "../assets/Rectangle11.png"
+import Rectangle1 from "../assets/Rectangle12.png"
+import Rectangle2 from "../assets/Rectangle13.png"
+import Rectangle3 from "../assets/Rectangle14.png"
+import Rectangle4 from "../assets/Rectangle15.png"
+import Rectangle5 from "../assets/Rectangle16.png"
+
+const service = [
+    {
+        image: Rectangle ,
+        title:"Стандартная терапия",
+        description:"Для облегчения похмельного синдрома и прерывания запоя. А также при алкогольном отравлении",
+        price:"2400",
+    },
+    {
+        image: Rectangle1 ,
+        title:"Усиленная терапия",
+        description:"Рекомендуется для облегчения похмелья и прерывания запоев длительностью до недели",
+        price:"5800",
+    },
+    {
+        image: Rectangle5 ,
+        title:"Восстановление +",
+        description:"Для облегчения похмельного синдрома и прерывания запоя. А также при алкогольном отравлении",
+        price:"15 800",
+    },
+    {
+        image: Rectangle3 ,
+        title:"Максимальная терапия",
+        description:"Для облегчения похмельного синдрома и прерывания запоя. А также при алкогольном отравлении",
+        price:"18 800",
+    },
+    {
+        image: Rectangle4 ,
+        title:"Кодирование +",
+        description:"Рекомендуется для облегчения похмелья и прерывания запоев длительностью до недели",
+        price:"3 200",
+    },
+    {
+        image: Rectangle2 ,
+        title:"Выезд нарколога на дом",
+        description:"Для облегчения похмельного синдрома и прерывания запоя. А также при алкогольном отравлении",
+        price:"3 500",
+    }
+];
+
+export default service;
