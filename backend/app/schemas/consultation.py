9292518e3a6921a4a7699db41ef0f0786f044f5d
maevska <sofiamaevskaa4@gmail.com>
2025-09-21 23:47:00 +0300

    modified:   backend/app/schemas/consultation.py 	modified:   backend/requirements.txt

--- backend/app/schemas/consultation.py
+++ backend/app/schemas/consultation.py
@@ -2,4 +2,4 @@ from pydantic import BaseModel, Field
 
 class ConsultationCreate(BaseModel):
     name: str = Field(..., min_length=2, max_length=50)
-    phone: str = Field(..., regex=r"^\+7 \d{3}-\d{3}-\d{2}-\d{2}$")
+    phone: str = Field(..., pattern=r"^\+7 \d{3}-\d{3}-\d{2}-\d{2}$")
